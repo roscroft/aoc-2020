02d7acd62f10391dd7fd471c456d02b374a5f8ef
Austin Herrling <adherrling@gmail.com>
2020-12-07 15:38:49 -0500

    Rewrote parsing with regexes instead of string splits.

--- aoc7.py
+++ aoc7.py
@@ -1,25 +1,21 @@
+import re
 import networkx as nx
 from utils import utils
 
-# regex = r"(\w+ \w+) bags contain (?:no other bags\.)|(?:(\d+) (\w+ \w+) bag(?:\.|s\.|s, ))+
+# regex = r"(\w+ \w+) bags contain (?:(?:no other bags\.)|(?:(\d+) (\w+ \w+) bag(?:\.|, |s\.|s, ))+)"
 
 def build_graph(data):
     DG = nx.DiGraph()
+    start_regex = re.compile(r"(\w+ \w+) bags")
+    end_regex = re.compile(r"(\d+) (\w+ \w+) bag")
     for line in data:
-        line = line.replace("bags", "bag")
-        line = line.replace(".", "")
-        # Start and end node(s) separated by " contain "
-        start_node, ends = line.split(" contain ")
-        # split end nodes on ", "
-        end_nodes = ends.split(", ")
+        # Start regex captures initial bag (no number on first bag)
+        start_node = start_regex.match(line).group(1)
+        # End regex captures later bags (with numbers, findall to get all)
+        end_nodes = end_regex.findall(line)
+        # end_nodes is a list of matches of the form [(weight, color)]
         for end_node in end_nodes:
-            if end_node != "no other bag":
-                end_node_data = end_node.split(" ")
-                edge_weight = int(end_node_data[0])
-                end_node = " ".join(end_node_data[1:])
-                # Digraph edges go from end to start to make problem solving easy. Adding 
-                # an edge between two nodes adds the two nodes to the node set (no duplicates).
-                DG.add_edge(end_node, start_node, weight=edge_weight)
+            DG.add_edge(end_node[1], start_node, weight=int(end_node[0]))
     return DG
 
 def part_1(DG, bag):
@@ -41,8 +37,9 @@ def part_2(DG, bag):
     return DG.nodes[bag]["weight"]
 
 if __name__ == "__main__":
-    data = utils.get_strs_from_file("data/aoc7_data.txt")
+    day = 7
+    data = utils.get_strs_from_file(f"data/aoc{day}_data.txt")
     DG = build_graph(data)
-    bag = "shiny gold bag"
+    bag = "shiny gold"
     print(f"Part 1 solution: {part_1(DG, bag)}")
     print(f"Part 2 solution: {part_2(DG, bag)}")
