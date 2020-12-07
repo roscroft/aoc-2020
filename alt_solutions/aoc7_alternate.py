37c221201bf071132865bc0e9da89553e6ec6016
Austin Herrling <adherrling@gmail.com>
2020-12-07 16:25:42 -0500

    Alternate recursion for alternate code

--- alt_solutions/aoc7_alternate.py
+++ alt_solutions/aoc7_alternate.py
@@ -6,23 +6,24 @@ def parse_rule(rule):
     outer_bag = re.search(r'(.*) bags contain', rule).group(1)
     inner_bag_list = re.findall(r'\d+ (.*?) bag', rule)
     return (outer_bag, inner_bag_list)
-def check_contains(bag_dict, bag_list, bag_color):
-    count = 0
-    for bag in bag_list:
-        if bag == bag_color:
-            count += 1
-            break
-        else:
-            count += check_contains(bag_dict, bag_dict.get(bag), bag_color)
-    return count > 0
+def check_contains(bag_dict, current_color, search_color):
+    if search_color in bag_dict[current_color]:
+        return 1
+    elif not bag_dict[current_color]:
+        # Empty bag
+        return 0
+    else:
+        for color in bag_dict[current_color]:
+            if check_contains(bag_dict, color, search_color):
+                return 1
+        return 0
 def calc_bags(rule_list, bag_color):
     bag_dict = dict((parse_rule(rule) for rule in rule_list))
-    print(bag_dict)
     count = 0
-    for bag in bag_dict:
-        count += check_contains(bag_dict, bag_dict.get(bag), bag_color)
+    for bag in bag_dict.keys():
+        count += check_contains(bag_dict, bag, bag_color)
     return count
 if __name__ == "__main__":
-    input = read_file("./data/aoc7_data2.txt")
+    input = read_file("./data/aoc7_data.txt")
     bag_color = "shiny gold"
     print(f"Part 1: {calc_bags(input, bag_color)}")
