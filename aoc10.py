e15df15598e1a1df2bd5a485a0f7804e757ea067
Austin Herrling <adherrling@gmail.com>
2020-12-10 17:46:36 -0500

    Day 10 solutions shortened

--- aoc10.py
+++ aoc10.py
@@ -1,26 +1,15 @@
 from utils import utils
 
 def part_1(data):
-    count_1 = 0
-    count_3 = 0
-    for i in range(1,len(data)):
-        if data[i] - data[i-1] == 1:
-            count_1 += 1
-        elif data[i] - data[i-1] == 3:
-            count_3 += 1
+    count_1 = sum([1 if data[i] - data[i-1] == 1 else 0 for i in range(len(data))])
+    count_3 = sum([1 if data[i] - data[i-1] == 3 else 0 for i in range(len(data))])
     return count_1*count_3
 
 def part_2(data):
-    dynm = [0] * len(data)
-    dynm[0] = 1
-    dynm[1] = 1
-    dynm[2] = 2 if data[2] - data[0] <= 3 else 1
-    for i in range(3, len(data)):
-        dynm[i] = sum([dynm[j] if data[i] - data[j] <= 3 else 0 for j in range(i-3,i)])
+    dynm = [1] + [0]*(len(data)-1)
+    for i in range(1, len(data)):
+        dynm[i] = sum([dynm[i-j] if data[i] - data[i-j] <= 3 else 0 for j in range(1, 4)])
     return dynm[-1]
-
-#def part_2_m(data):
-
 
 if __name__ == "__main__":
     day = 10
